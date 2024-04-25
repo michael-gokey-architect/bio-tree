6d827bb04745439f878ea7471fa39d34c5541d6c
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-04-24 21:09:07 -0400

    adding routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './guest/1-welcome/welcome.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
